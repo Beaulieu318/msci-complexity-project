944d9cf2dfce82d7d37987d762fdae3cef856553
Sebastian Lee <sebalexlee@gmail.com>
2017-10-11 12:43:29 +0100

    changed from self-made dictionaries to keeping in pandas

--- msci/analysis/initial_data_analysis.py
+++ msci/analysis/initial_data_analysis.py
@@ -5,57 +5,56 @@ matplotlib.style.use('ggplot')
 import numpy as np
 import math
 
-#shopper_df = pd.read_csv('../data/bag_mus_12-22-2016.csv')
-#shopper_df.date_time = shopper_df.date_time.astype('datetime64[ns]')
 
 class DataSet:
 
     def __init__(self, df):
         self.df = df
-        self.data_dictionary = {}
 
     def data_anatomy(self):
+        """
+        Basic analysis of compositions of data set
+        :return: number of signals, number of unique signals, number of unique IDs
+        """
         rows = len(self.df)
         unique_rows = len(self.df.drop_duplicates())
         unique_ids = len(self.df['mac_address'].drop_duplicates())
         return rows, unique_rows, unique_ids
 
     def volume_analysis(self):
+        """
+        Analysis of signals over time
+        :return: outputs histograms of customer volume over time
+        """
         fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 6))
         ax = self.df.date_time.hist(bins=50)
         ax.set_title('Histogram of shoppers against time')
         ax.set_xlabel('Time (month-day hour)')
         ax.set_ylabel('Count of shoppers (no.)')
         fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 6))
-        ax = self.df.loc[shopper_df.drop_duplicates('mac_address').index].date_time.hist(bins=50)
+        ax = self.df.loc[self.df.drop_duplicates('mac_address').index].date_time.hist(bins=50)
         ax.set_title('Histogram of shoppers against time')
         ax.set_xlabel('Time (month-day hour)')
         ax.set_ylabel('Count of shoppers (no.)')
 
-    def signal_numbers(self):
-        dictionary = self.df_to_dictionary()
-        unique_id_list = self.df['mac_address'].drop_duplicates().tolist()
-        signal_magnitudes = [len(dictionary[i]) for i in unique_id_list]
-        return unique_id_list, signal_magnitudes
+    def group_df(self):
+        """
+        groups dataframe by mac address to isolate movements of individuals
+        :return: grouped data frame
+        """
+        mac_df = self.df.groupby('mac_address')
+        return mac_df
 
-    def sort_data_frame(self):
-        sorted_df = self.df.sort_values(['mac_address'])
-        positions = list(zip(sorted_df['x'], sorted_df['y']))
-        macs = np.array(sorted_df['mac_address'].tolist())
-        changes = np.where(macs[:-1] != macs[1:])[0]
-        changes = np.insert(changes, 0, 0)
-        return sorted_df, positions, macs, changes
-
-    def df_to_dictionary(self):
-        sorted_data = self.sort_data_frame()
-        positions = sorted_data[1]
-        macs = sorted_data[2]
-        changes = sorted_data[3]
-        signal_magnitudes = []
-        for i in range(len(changes)-1):
-            self.data_dictionary[macs[changes[i]+1]] = positions[changes[i]:changes[i+1]]
-            signal_magnitudes.append(len(self.data_dictionary[macs[changes[i]+1]]))
-        return self.data_dictionary, signal_magnitudes
+    def size_analysis(self, mac_df):
+        """
+        Analysis of distributions of signal numbers
+        :param mac_df: mac sorted data frame
+        :return: mean, std of signals sizes
+        """
+        sizes = mac_df.size()
+        mean_size = np.mean(sizes)
+        std_size = np.std(sizes)
+        return mean_size, std_size
 
     def all_mac_indices(self, mac_index):
         macs = self.df['mac_address'].tolist()
