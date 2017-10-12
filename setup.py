5598b4dc32144704244a18c5e5c8e6036e583965
Tom du Val <tvb@apsleyfarms.com>
2017-10-12 17:35:22 +0100

    add setup file and init files
    Add the setup file to enable code to be called from everywhere within the repo.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msci',
+    version='0.1',
+    long_description='Msci Complexity Project.',
+    packages=find_packages(),
+    install_requires=[],
+)
