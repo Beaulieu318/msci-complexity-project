fc5eea569c2fc83989269f7c45af47e2c50eff09
Tom du Val <tvb@apsleyfarms.com>
2017-10-12 17:54:15 +0100

    move files into msci folder
    All folder can be accessed using an import msci statement then e.g. msci.analysis

---
+++ msci/cleaning/test.py
@@ -0,0 +1,1 @@
+from msci.cleaning import test
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='msci',
+    name='msci_complexity',
     version='0.1',
     long_description='Msci Complexity Project.',
     packages=find_packages(),
