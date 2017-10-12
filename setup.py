8442f694125b3d484371d14faf88c7dd103a82c0
Tom du Val <tvb@apsleyfarms.com>
2017-10-12 17:55:06 +0100

    change name in setup to msci_complexity_project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='msci_complexity',
+    name='msci_complexity_project',
     version='0.1',
     long_description='Msci Complexity Project.',
     packages=find_packages(),
