27134514b4074670185d5f010274b3b8c9426ee9
Tom du Val <tvb@apsleyfarms.com>
2017-10-16 17:50:35 +0100

    refactor clean data
    Convert clean data into functions from a class and replace some python code with pandas calls

--- cleaning/clean_data.py
+++ cleaning/clean_data.py
@@ -1,102 +1,78 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 
-columns = ['mac_address', 'date_time', 'location', 'floor', 'x', 'y']
-
-shopper_df = pd.read_csv('../data/bag_mus_12-22-2016.csv', usecols=columns)
-shopper_df.date_time = shopper_df.date_time.astype('datetime64[ns]')
-hl_df = shopper_df[shopper_df['location'] == 'Home & Leisure']
-mm_df = shopper_df[shopper_df['location'] == 'Mall of Mauritius']
-p_df = shopper_df[shopper_df['location'] == 'Phoenix Mall']
+COLUMNS_TO_IMPORT = ['mac_address', 'date_time', 'location', 'floor', 'x', 'y']
 
 
-def clean_all_data(minimum, speed, open_time, close_time):
-    p_ds = DataSet(p_df, open_time, close_time)
-    m_ds = DataSet(mm_df, open_time, close_time)
-    h_ds = DataSet(hl_df, open_time, close_time)
-    p_ds.clean(minimum, speed)
-    m_ds.clean(minimum, speed)
-    h_ds.clean(minimum, speed)
-    p_ds.export_csv('phoenix')
-    m_ds.export_csv('mauritius')
-    h_ds.export_csv('homeleisure')
+def remove_duplicates(shopper_df):
+    """
+    removes identical signals that are clearly errant duplicates i.e. same time and place for a given mac_id
+
+    :param shopper_df: (pd.DataFrame) the signals of the shoppers
+    :return: (pd.DataFrame) the cleaned signals of the shoppers
+    """
+    shopper_unique_df = shopper_df.drop_duplicates()
+    return shopper_unique_df
 
 
-class DataSet:
+def remove_outside_hours(shopper_df, open_time, close_time):
+    """
+    removes mac addresses that are received outside opening hours
 
-    def __init__(self, df, open_time, close_time):
-        """
-        :param df: the data frame to be cleaned
-        :param open: ('hh:mm:ss') opening time of mall
-        :param close: ('hh:mm:ss') closing time of mall
-        """
-        self.df = df
-        self.open = open_time
-        self.close = close_time
+    :param shopper_df: (pd.DataFrame) the signals of the shoppers
+    :param open_time: ('hh:mm:ss') opening time of mall
+    :param close_time: ('hh:mm:ss') closing time of mall
+    :return: (pd.DataFrame) the cleaned signals of the shoppers
+    """
+    date_index_df = shopper_df.copy()
+    date_index_df.index = date_index_df.date_time.astype('datetime64[ns]')
+    signal_out_of_hours = date_index_df.between_time(close_time, open_time)
+    mac_address_out_of_hours = signal_out_of_hours.mac_address.drop_duplicates().tolist()
+    shopper_inside_hours_df = shopper_df[~shopper_df.mac_address.isin(mac_address_out_of_hours)]
+    return shopper_inside_hours_df
 
-    def remove_duplicates(self):
-        """
-        removes identical signals that are clearly errant duplicates i.e. same time and place for a given mac_id
-        """
-        self.df = self.df.drop_duplicates()
 
-    def remove_outside_hours(self):
-        """
-        removes mac addresses that are received outside opening hours
+def remove_sparse_data(shopper_df, minimum):
+    """
+    removes mac_ids that have too few data points to be of use
 
-        :return: legitimate mac ids i.e. those that are received during opening hours
-        """
-        signal_out_of_hours = self.df[self.df.date_time.dt.strftime('%H:%M:%S').between(self.open, self.close)]
-        mac_address_out_of_hours = signal_out_of_hours.mac_address.drop_duplicates().tolist()
-        in_time_df = self.df[~self.df.mac_address.isin(mac_address_out_of_hours)]
-        self.df = in_time_df
-        print('Number of data points left:', len(self.df))
-        mac_address_in_hours = self.df.mac_address.drop_duplicates().tolist()
-        return mac_address_in_hours
+    :param shopper_df: (pd.DataFrame) the signals of the shoppers
+    :param minimum: the threshold for number of data points for a data set to be kept
+    :return: (pd.DataFrame) the cleaned signals of the shoppers
+    """
+    mac_group = shopper_df.groupby('mac_address')
+    mac_address_sparse = mac_group.size()[mac_group.size() > minimum].index.tolist()
+    shopper_large_data_df = shopper_df[~shopper_df.mac_address.isin(mac_address_sparse)]
+    return shopper_large_data_df
 
-    def remove_sparse_data(self, minimum):
-        """
-        removes mac_ids that have too few data points to be of use
 
-        :param minimum: the threshold for number of data points for a data set to be kept
-        """
-        mac_group = self.df.groupby('mac_address')
-        sizes = mac_group.size()
-        thresh = [sizes < minimum]
-        sizes_index = sizes.index.tolist()
-        mac_address_sparse = [sizes_index[i] for i in range(len(sizes)) if thresh[0][i]]
-        self.df = self.df[~self.df.mac_address.isin(mac_address_sparse)]
-        print('Number of data points left:', len(self.df))
+def remove_unrealistic_speeds(shopper_df, speed):
+    """
+    removes mac ids that are moving too fast to be pedestrian movement
 
-    def remove_unrealistic_speeds(self, speed):
-        """
-        removes mac ids that are moving too fast to be pedestrian movement
+    :param shopper_df: (pd.DataFrame) the signals of the shoppers
+    :param speed: max speed allowed for pedestrian
+    :return: (pd.DataFrame) the cleaned signals of the shoppers
+    """
+    shopper_df.date_time = shopper_df.date_time.astype('datetime64[ns]')
+    time_sorted = shopper_df.sort_values('date_time')
+    mac_group = time_sorted.groupby('mac_address')
 
-        :param speed: max speed allowed for pedestrian
-        """
-        macs = self.remove_outside_hours()
-        time_sorted = self.df.sort_values('date_time')
-        mac_group = time_sorted.groupby('mac_address')
-        mac_too_fast = []
-        mac_speeds = []
-        for mac in macs:
-            mac_dp = mac_group.get_group(mac)
-            speeds = _speed_of_group(mac_dp)
-            speeds = speeds[speeds < 100000]
-            if np.mean(speeds)>speed:
-                mac_too_fast.append(mac)
-            mac_speeds.append(speeds)
-        self.df = self.df[~self.df.mac_address.isin(mac_too_fast)]
+    # Remove a single mac address (can't calculate speed)
+    macs = mac_group.size()[mac_group.size() > 1].index.tolist()
 
-    def clean(self, minimum, speed):
-        self.remove_duplicates()
-        self.remove_sparse_data(minimum)
-        self.remove_unrealistic_speeds(speed)
+    mac_too_fast = []
+    mac_speeds = []
 
-    def export_csv(self, mall_name):
-        self.df.to_csv(path_or_buf='../data/clean_data_' + mall_name + '.csv', columns=columns)
+    for mac in macs:
+        mac_dp = mac_group.get_group(mac)
+        speeds = _speed_of_group(mac_dp)
+        speeds = speeds[speeds < 100000]
+        if np.mean(speeds) > speed:
+            mac_too_fast.append(mac)
+        mac_speeds.append(speeds)
+    shopper_good_speeds_df = shopper_df[~shopper_df.mac_address.isin(mac_too_fast)]
+    return shopper_good_speeds_df
 
 
 def _speed_of_group(mac_dp):
@@ -136,3 +112,40 @@ def _time_difference(t0, t1):
     """
     tdelta = t1 - t0
     return tdelta.seconds
+
+
+def clean(shopper_df):
+    """
+    cleans the dataframe containing the signals of the shoppers by:
+     - removing duplicates
+     - removing mac addresses with signals outside of shopping hours
+     - removing mac addresses with few signals
+     - removing shoppers with unrealistic speeds
+
+    :param shopper_df: (pd.DataFrame) the signals of the shoppers
+    :return: (pd.DataFrame) the cleaned signals of the shoppers
+    """
+    shopper_df = remove_duplicates(shopper_df)
+    shopper_df = remove_outside_hours(shopper_df, '08:00:00', '22:00:00')
+    shopper_df = remove_sparse_data(shopper_df, 10)
+    shopper_df = remove_unrealistic_speeds(shopper_df, 10)
+    return shopper_df
+
+
+def main():
+    shopper_df = pd.read_csv('../data/bag_mus_12-22-2016.csv', usecols=COLUMNS_TO_IMPORT)
+    shopper_df.date_time = shopper_df.date_time.astype('datetime64[ns]')
+
+    hl_df = shopper_df[shopper_df['location'] == 'Home & Leisure']
+    mm_df = shopper_df[shopper_df['location'] == 'Mall of Mauritius']
+    p_df = shopper_df[shopper_df['location'] == 'Phoenix Mall']
+
+    locations = {'Home & Leisure': hl_df, 'Mall of Mauritius': mm_df, 'Phoenix Mall': p_df}
+
+    for location, shopper_dirty_df in locations.items():
+        shopper_cleaned_df = clean(shopper_dirty_df)
+        shopper_cleaned_df.to_csv(path_or_buf='../data/clean_data_' + location + '.csv', columns=COLUMNS_TO_IMPORT)
+
+
+if __name__ == '__main__':
+    main()
