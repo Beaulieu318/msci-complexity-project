ab37d0f045f925dcd48851cba439c5735f96a1c1
Tom du Val <tvb@apsleyfarms.com>
2017-11-10 13:52:15 +0000

    add modules script which automatically imports relevant modules
    The module also sets autoreload working
    
    Type: from msci.utils.modules import *

---
+++ msci/utils/modules.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from msci.utils import utils, plot
+
+from IPython import get_ipython
+ipython = get_ipython()
+ipython.magic('load_ext autoreload')
+ipython.magic('autoreload 2')
