9ae52baa66e14fb7f667cd7dc261989ad52233f6
Tom du Val <tvb@apsleyfarms.com>
2018-02-04 18:30:04 +0000

    add shopper similarity network into networks.py

--- msci/analysis/networks.py
+++ msci/analysis/networks.py
@@ -162,3 +162,40 @@ def calculate_in_degree_rank_probability(total_shopper_count_df):
         rank.append(r_i)
 
     return degrees, rank
+
+
+def create_shopper_shop_pivot(signal_df):
+    clean_signal_df = signal_df[
+        signal_df.store_id.notnull() &
+        signal_df.store_id.str.contains('B')
+    ]
+    shopper_shop_pivot = pd.pivot_table(clean_signal_df, values='x', index='mac_address', columns='store_id', aggfunc=len)
+    return shopper_shop_pivot
+
+
+def create_similarity_matrix(shopper_shop_df, num_entities=100, skip_entities=0):
+    """
+    Creates a shop or shopper similarity matrix.
+    The x-axis is entities which the similarity is based on and y-axis is overlap which is being counted.
+    E.g. a shopper shopper matrix will contain the number shops both shopper_i and shopper_j both went to
+    E.g. a shop shop matrix will contain the number of shoppers that both went to shop_i and shop_j
+
+    :param shopper_shop_df: (pd.DataFrame) the shoppers on one axis with the shops on the other
+    :return: (pd.DataFrame) entity entity matrix with the number of overlap in the value (entity is shop or shopper)
+    """
+    shopper_shop_df[shopper_shop_df > 0] = 1
+    # shopper_shop_df = shopper_shop_df[shopper_shop_df.notnull().sum(axis=1) == 5]
+    shopper_shop_index_df = (shopper_shop_df * np.arange(shopper_shop_df.shape[1]))[
+                            skip_entities:skip_entities + num_entities]
+    similarity_df = pd.DataFrame(0, shopper_shop_index_df.index, shopper_shop_index_df.index)
+
+    for i in range(len(similarity_df.index)):
+        entity_index_i = shopper_shop_index_df.iloc[i][shopper_shop_index_df.iloc[i].notnull()]
+        for j in range(i + 1, (len(similarity_df.index))):
+            entity_index_j = shopper_shop_index_df.iloc[j][shopper_shop_index_df.iloc[j].notnull()]
+            num_overlap = len(list(set(entity_index_i) & set(entity_index_j)))
+            similarity_df.iloc[i, j] = num_overlap
+
+    norm = shopper_shop_index_df.notnull().sum(axis=1)
+
+    return similarity_df / norm + similarity_df.T / norm
