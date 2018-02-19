7bf8d12ea5b468e16fb9c120e0d26f652254d896
Tom du Val <tvb@apsleyfarms.com>
2018-02-19 17:53:00 +0000

    update import for mac address df to version 3

--- msci/utils/utils.py
+++ msci/utils/utils.py
@@ -97,6 +97,10 @@ def import_mac_addresses(mall='Mall of Mauritius', version=1, signal_type=None):
         mac_address_df = pd.read_csv(
             dir_path + '/../data/{}_featuresv2.csv'.format(malls[mall])
         )
+    elif version == 3:
+        mac_address_df = pd.read_csv(
+            dir_path + '/../data/{}_featuresv3.csv'.format(malls[mall])
+        )
     else:
         raise Exception("There is no version with this number. Please choose either 1 or 2!")
 
